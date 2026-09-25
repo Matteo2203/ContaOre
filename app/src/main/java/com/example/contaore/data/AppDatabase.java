9c1cca633872456fdaad0b6de3b02c4de79f023b
Matteo2203 <l.matteo2003@gmail.com>
2026-09-25 12:48:54 +0200

    Aggiunta classe AppDatabase con pattern Singleton

---
+++ app/src/main/java/com/example/contaore/data/AppDatabase.java
@@ -0,0 +1,25 @@
+package com.example.contaore.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {WeekEntry.class}, version = 1 )
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract  WeekEntryDao weekEntryDao();
+    static AppDatabase i;
+
+    static AppDatabase getInstance(Context context){
+        if(i == null)
+            i = Room.databaseBuilder(context, AppDatabase.class , "orari").build();
+
+        return i;
+
+    }
+
+
+
+}
